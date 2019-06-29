148c9a8707a5a3e1e4bd427c2669cb4b24bb90aa
Felipe Rust <rustfelipe@gmail.com>
2019-06-29 14:49:13 -0400

    update

--- Grid_plot.R
+++ Grid_plot.R
@@ -3,4 +3,8 @@ library(cowplot)
 
 load("lrdata.RData")
 
-
+data <- subset(lrdata, subset = series %in%  c("LR") & 
+                 year %in%  c(2015)   & distance.from.mouth > 154 &
+                 sample.type %in%  c("HeadwaterLakes","Reservoir", "River", "Tributary", 
+                                     "Stream"),
+               select = c("Rn", "distance.from.mouth", "pco2.headspace"))
